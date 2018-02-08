8e04cebc5a040056db3dca4f2564adebd503d1e5
koenvaneijk <vaneijk.koen@gmail.com>
2018-02-08 16:00:02 +0100

    updated readme and added configuration wizard script

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Setup
+# Automagica Client
+
+## Install
+
+### Prerequisites
+Python 3.6.4 from https://www.python.org
+
+### Instructions
 Install Automagica on the robot host machine:
 ```
 pip install git+https://github.com/OakwoodAI/automagica
 ```
+## Run
+In command line or terminal run following command:
+```
+automagica
+```
+### First run
+The configuration wizard will ask for your Automagica Robot ID. Please visit https://automagica.io to register your robot and receive your Automagica Robot ID.
--- automagica/__init__.py
+++ automagica/__init__.py
@@ -5,11 +5,23 @@ from socketIO_client import SocketIO
 from .auth import Auth
 from .robot import Robot
 
+def config():
+    if not os.environ.get('AUTOMAGICA_ROBOT_ID'):
+        automagica_robot_id = None
+        while not automagica_robot_id:
+            automagica_robot_id = input('Please provide the Automagica Robot ID: \n')
+        os.environ['AUTOMAGICA_ROBOT_ID'] = automagica_robot_id
+    if not os.environ.get('AUTOMAGICA_HOST'):
+        automagica_host = input('Please provide the Automagica host (leave blank for https://automagica.io): \n')
+        if not automagica_host:
+            automagica_host = 'https://automagica.io'
+        os.environ['AUTOMAGICA_HOST'] = automagica_host
 
 def start():
     os.system('cls')
-    print('Automagica Robot launched and waiting!')
-    socketIO = SocketIO('localhost', 8000)
+    config()
+    print('Automagica Robot launched and waiting! Listening on port 7175 for commands.')
+    socketIO = SocketIO(os.environ['AUTOMAGICA_HOST'], 7175)
     robot_namespace = socketIO.define(Robot, '/robot')
     socketIO.wait()
 
