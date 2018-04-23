9b6a1087c83b6a467af2f8414ce418a947363c5e
Koen van Eijk <koen@oakwood.ai>
2018-04-23 14:57:23 +0200

    removed socketio client from the library. desktop client fulfils this requirement.

--- automagica/__init__.py
+++ automagica/__init__.py
@@ -1,45 +1,9 @@
-import os
 import sys
-from socketIO_client import SocketIO
 
-from .auth import Auth
-from .bot import Bot
 from .activities import *
 
-
 def start():
-    os.system('cls')
-
-    if len(sys.argv) > 1:
-        bot_id = sys.argv[1]
-    else:
-        bot_id = input('Please provide your Automagica Bot ID: \n')
-
-    if len(sys.argv) > 2:
-        host = sys.argv[2]
-    else:
-        host = 'https://portal.automagica.be'
-
-    if len(sys.argv) > 3:
-        port = sys.argv[3]
-    else:
-        port = None
-
-    
-    socketIO = SocketIO(host, port)
-    bot = Bot
-    bot.bot_id = bot_id
-    
-    while True:
-        try:
-            bot_namespace = socketIO.define(bot, '/bot')
-            break
-        except:
-            os.system('cls')
-            print('Connecting...')
-            pass
-    
-    socketIO.wait()
+    exec(sys.argv[1], globals())
     
 if __name__ == '__main__':
     start()
--- automagica/auth.py
+++
@@ -1,7 +0,0 @@
-from socketIO_client import BaseNamespace
-import os
-
-
-class Auth(BaseNamespace):
-    def on_connect(self):
-        self.emit('auth', os.environ['AUTOMAGICA_ROBOT_ID'])
