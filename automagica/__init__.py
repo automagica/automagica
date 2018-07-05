418dddff055c05dfb635cc7f2659823dd7cb7ea4
Koen van Eijk <vaneijk.koen@gmail.com>
2018-07-05 12:29:09 +0200

    added argparse and optional parameters argument

--- automagica/__init__.py
+++ automagica/__init__.py
@@ -2,8 +2,23 @@ import sys
 
 from .activities import *
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Automagica CLI')
+
+parser.add_argument('script', type=str,
+                    help='Script for the Automagica Bot')
+
+parser.add_argument('--parameters', default='',
+                    help='Parameters for the Automagica Bot')
+
 def start():
-    exec(sys.argv[1], globals())
-    
+    # Retrieve arguments
+    args = parser.parse_args()
+    # Run parameters first
+    exec(args.parameters, globals())
+    # Run script
+    exec(args.script, globals())
+
 if __name__ == '__main__':
     start()
