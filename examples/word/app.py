3f8087341d11ae7cfccb0931ec3c7e7e431b9811
PetWroak <41777755+PetWroak@users.noreply.github.com>
2018-07-27 09:50:41 +0200

    Update app.py
    small update

--- examples/word/app.py
+++ examples/word/app.py
@@ -1,13 +1,12 @@
 from automagica import *
+import os
 
 """
 Opens a template Word document, replaces a template with content and saves as a new file.
 """
 
 document = OpenWordDocument('example.docx')
-
 new_document = ReplaceText(document, text='[template]', replace_with='invoice 123456')
-
 new_document.save('result.docx')
 
 
@@ -16,10 +15,6 @@ Opens the new file and saves it as PDF (all in the background)
 This only works on Windows with Microsoft Office installed!
 """
 
-import os
-
 full_path_word_file = os.path.join(os.getcwd(), 'result.docx')
-
 full_path_pdf_file = full_path_word_file.replace('.docx', '.pdf')
-
 ConvertWordToPDF(word_filename=full_path_word_file, pdf_filename=full_path_pdf_file)
