fe540c0bff082935a8ead57d38a30c53acc83cb8
tvturnhout <tvturnhout@gmail.com>
2020-01-10 11:07:26 +0100

    new subversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="Automagica",
-    version="2.0.2",
+    version="2.0.3",
     description="Bot for Automagica",
     author="Oakwood Technologies BVBA",
     author_email="mail@oakwood.ai",
