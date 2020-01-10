b5f513d73653bd68f65ac403268c6ea21fa47638
tvturnhout <tvturnhout@gmail.com>
2020-01-10 11:33:34 +0100

    version increase

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="Automagica",
-    version="2.0.3",
+    version="2.0.4",
     description="Bot for Automagica",
     author="Oakwood Technologies BVBA",
     author_email="mail@oakwood.ai",
