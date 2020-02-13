e9483a9924d8ee1c9b887676b56a6172ce4a9d5e
tvturnhout <tvturnhout@gmail.com>
2020-02-13 10:32:37 +0100

    version increase

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ package_data = {
 
 setup(
     name="Automagica",
-    version="2.0.20",
+    version="2.0.21",
     description="Bot for Automagica",
     author="Oakwood Technologies BVBA",
     author_email="mail@oakwood.ai",
