cc6f51dbcc59c0f44df441b5814476c9b0dc240b
Koen van Eijk (Oakwood) <koen@oakwood.ai>
2020-04-03 13:39:32 +0200

    Updated setup.py for Recorder requirements

--- setup.py
+++ setup.py
@@ -57,7 +57,8 @@ install_requires = [
     "jupyterlab==1.2.4",  # BSD 3-Clause
     "py-trello==0.13.0",  # BSD 3-Clause "New" or "Revised" License
     "plyer==1.4.0",  # MIT License
-    "Pillow==7.0.0",  # PIL License (permissive)
+    "Pillow==7.0.0",  # PIL License (permissive),
+    "pyperclip==1.7.0",  # BSD License (BSD)
     "PyAutoGUI==0.9.48",  # BSD 3-Clause "New" license
     "pysnmp==4.4.12",  # BSD 2-Clause "Simplified" License
     "pandas==1.0.0",  # BSD 3-Clause
@@ -79,17 +80,17 @@ package_data = {
         # Copyrighted by Oakwood Technologies BVBA
         "lab/.jupyter/custom/logo-white.png",
         "icon.ico",  # Copyrighted by Oakwood Technologies BVBA
-        "icons/automagica.ico" # Copyrighted by Oakwood Technologies BVBA
-        "icons/click_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/double_click_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/is_visible_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/middle_click_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/move_to_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/read_text_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/right_click_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/type_into_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/wait_appear_button.png", # Copyrighted by Oakwood Technologies BVBA
-        "icons/wait_vanish_button.png", # Copyrighted by Oakwood Technologies BVBA        
+        "icons/automagica.ico"  # Copyrighted by Oakwood Technologies BVBA
+        "icons/click_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/double_click_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/is_visible_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/middle_click_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/move_to_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/read_text_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/right_click_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/type_into_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/wait_appear_button.png",  # Copyrighted by Oakwood Technologies BVBA
+        "icons/wait_vanish_button.png",  # Copyrighted by Oakwood Technologies BVBA
     ]
 }
 
