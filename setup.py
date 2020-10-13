b71b976f87209d1e68909499c196c8d96d142734
tvturnhout <tvturnhout@gmail.com>
2020-10-13 17:54:28 +0200

    docs redirect

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Automagica',
+    version='0.0.1',
+    url='https://github.com/automagica/automagica',
+    author='Oakwood Technologies BVBA',
+    author_email='info@netcall.com',
+    description='Smart Robotic Process Automation',
+    packages=find_packages(),    
+)
