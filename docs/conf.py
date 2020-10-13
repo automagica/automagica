79b56dcccc1aabf049dfcd63689814402618581c
tvturnhout <tvturnhout@gmail.com>
2020-10-13 17:50:58 +0200

    redirect documentation

---
+++ docs/conf.py
@@ -0,0 +1,6 @@
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {'.md': CommonMarkParser}
+
+source_suffix = ['.md']
+master_doc = 'index'
---
+++ index.md
@@ -0,0 +1,1 @@
+<meta http-equiv="refresh" content="0; URL=http://automagica.com/documentation/">`
